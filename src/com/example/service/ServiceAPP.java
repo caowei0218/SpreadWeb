76804c0118212cd1620121df09ac8ed3102d020e
曹溦 <caowei0218@qq.com>
2015-07-17 15:18:33 +0800

    登陆接口搞定

--- src/com/example/service/ServiceAPP.java
+++ src/com/example/service/ServiceAPP.java
@@ -12,11 +12,13 @@ public class ServiceAPP {
 		return user;
 	}
 
-	public UserBean login(UserBean user) {
+	public String login(String userName, String password) {
 		text = new TextFile();
-		text.string2File(user.toString(), "D:\\Spread.txt");
+		// text.string2File(userBean.toString(), "D:\\Spread.txt");
+		text.string2File(userName + ":" + password, "D:\\Spread.txt");
 		System.out.println("登陆接口访问成功");
-		return user;
+		
+		return "ok";
 	}
 
 }
